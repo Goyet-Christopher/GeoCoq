1d594d7c1c0dd17ab37c39068d78244b470145df
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-11-04 20:30:34 +0100

    Add files via upload

--- Tarski_dev/Ch05_bet_le/Ch05_final.v
+++ Tarski_dev/Ch05_bet_le/Ch05_final.v
@@ -1,7 +1,9 @@
 Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_le.
 Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_bet_le.
 Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_bet_cong.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_cong_decidability.
 Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_lt.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_lt_not.
 Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_ge.
 Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_gt.
 (* Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_tactics. *)
--- Tarski_dev/Ch05_bet_le/Ch05_gt.v
+++ Tarski_dev/Ch05_bet_le/Ch05_gt.v
@@ -1,26 +1,48 @@
 Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_ge.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_lt.
 
 Section Gt_prop.
 Context `{TnEQD:Tarski_neutral_dimensionless_with_decidable_point_equality}.
 
 
-Lemma gt_left_comm : forall A B C D, Gt A B C D -> Gt B A C D.
+Lemma gt_to_def : forall A B C D,
+  Gt A B C D -> Lt C D A B.
 Proof.
     intros.
-    unfold Gt in *.
+    assumption.
+Qed.
+
+Lemma def_to_gt : forall A B C D,
+  Lt C D A B -> Gt A B C D.
+Proof.
+    intros.
+    assumption.
+Qed.
+
+Lemma gt_left_comm : forall A B C D,
+  Gt A B C D -> Gt B A C D.
+Proof.
+    intros.
+    apply def_to_gt.
     apply lt_right_comm.
     assumption.
 Qed.
 
-Lemma gt_right_comm : forall A B C D, Gt A B C D -> Gt A B D C.
+Definition gt_2134 A B C D := gt_left_comm A B C D.
+
+Lemma gt_right_comm : forall A B C D,
+  Gt A B C D -> Gt A B D C.
 Proof.
     intros.
-    unfold Gt in *.
+    apply def_to_gt.
     apply lt_left_comm.
     assumption.
 Qed.
 
-Lemma gt_comm : forall A B C D, Gt A B C D -> Gt B A D C.
+Definition gt_1243 A B C D := gt_right_comm A B C D.
+
+Lemma gt_comm : forall A B C D,
+  Gt A B C D -> Gt B A D C.
 Proof.
     intros.
     apply gt_left_comm.
@@ -28,4 +50,10 @@ Proof.
     assumption.
 Qed.
 
+Definition gt_2143 A B C D := gt_comm A B C D.
+
 End Gt_prop.
+
+Print All.
+
+
