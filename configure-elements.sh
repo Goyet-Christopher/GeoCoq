5886c5900f8b222552c16d322d7a2e840af1f675
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-11-04 19:58:16 +0100

    Delete configure-elements.sh

--- configure-elements.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-cp -f _CoqProject.in _CoqProject
-find . -name "*.v" | grep OriginalProofs >> _CoqProject
-find . -name "*.v" | grep euclid_to_tarski >> _CoqProject
-coq_makefile -f _CoqProject -o Makefile
-rm -f .coqdeps.d
