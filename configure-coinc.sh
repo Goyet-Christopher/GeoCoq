02136bcc78dce5dc28b3d7599afea7ac94e67485
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-11-04 19:57:55 +0100

    Delete configure-coinc.sh

--- configure-coinc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-cp -f _CoqProject.in _CoqProject
-find . -name "*.v" | grep "Tactics/Coinc/" >> _CoqProject
-find . -name "*.v" | grep Utils >> _CoqProject
-coq_makefile -f _CoqProject -o Makefile
-rm -f .coqdeps.d
