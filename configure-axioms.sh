486ceddde73e6210de65f53b62b3b859c5664ab5
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-11-04 19:57:33 +0100

    Delete configure-axioms.sh

--- configure-axioms.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-cp -f _CoqProject.in _CoqProject
-find . -name "*.v" | grep Axioms >> _CoqProject
-find . -name "*.v" | grep Definitions >> _CoqProject
-find . -name "*.v" | grep finish >> _CoqProject
-coq_makefile -f _CoqProject -o Makefile
-rm -f .coqdeps.d
