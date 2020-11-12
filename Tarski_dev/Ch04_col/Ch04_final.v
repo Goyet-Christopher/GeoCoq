3abb4d5b41d4d753e1e63c19cd9e5ba69a866625
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-11-12 18:54:22 +0100

    Add files via upload

--- Tarski_dev/Ch04_col/Ch04_final.v
+++ Tarski_dev/Ch04_col/Ch04_final.v
@@ -1,4 +1,5 @@
 Require Export GeoCoq.Tarski_dev.Ch04_col.Ch04_col.
+Require Export GeoCoq.Tarski_dev.Ch04_col.Ch04_col_transitivity.
 Require Export GeoCoq.Tarski_dev.Ch04_col.Ch04_col_not.
 Require Export GeoCoq.Tarski_dev.Ch04_col.Ch04_col_exists.
 Require Export GeoCoq.Tarski_dev.Ch04_col.Ch04_FSC.
