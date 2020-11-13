e300888cf6742093eb05fc20990b56c217f36025
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-11-13 22:27:46 +0100

    Create Ch05_final.v

---
+++ Tarski_dev/Ch05_le_lt_ge_gt/Ch05_final.v
@@ -0,0 +1,10 @@
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_le.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_bet_le.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_bet_cong_eq.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_cong_decidability.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_lt.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_lt_not.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_bet_lt.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_ge.
+Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_gt.
+(* Require Export GeoCoq.Tarski_dev.Ch05_bet_le.Ch05_tactics. *)
