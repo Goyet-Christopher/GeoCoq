ad76133b46849133794abeb9ba573d9fec3ca7b4
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-11-13 22:45:41 +0100

    Delete Ch05_tactics.v

--- Tarski_dev/Ch05_bet_le/Ch05_tactics.v
+++
@@ -1,5 +0,0 @@
-
-Hint Resolve le_reflexivity le_anti_symmetry le_trivial le_zero cong__le cong__le3412
-             le1221 le_left_comm le_right_comm le_comm lt__le bet__le1213 bet__le2313 : Le.
-
-Ltac Le := auto with Le.
