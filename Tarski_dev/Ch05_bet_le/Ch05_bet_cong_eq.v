5c4a5232999e621c2171a2260d592ded16172252
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-11-13 17:44:20 +0100

    Add files via upload

--- Tarski_dev/Ch05_bet_le/Ch05_bet_cong_eq.v
+++ Tarski_dev/Ch05_bet_le/Ch05_bet_cong_eq.v
@@ -27,6 +27,40 @@ Proof.
       apply cong_3412; assumption.
 Qed.
 
+Lemma bet_cong_eq2 : forall A B C A' B' C',
+  Bet A B C -> Cong A B A' B' -> Cong A C A' C'
+  -> Bet A' C' B' -> B = C /\ B' = C'.
+Proof.
+    intros.
+    assert (Le A' C' A' B').
+      apply def2_to_le.
+      exists B'.
+      split.
+        assumption.
+        apply cong_1212.
+    assert(Le A' B' A' C').
+      apply l5_6 with A B A C.
+        apply def2_to_le.
+          exists C.
+          split.
+            assumption.
+            apply cong_1212; assumption.
+        assumption.
+        assumption.
+    assert(Cong A' B' A' C').
+      apply le_anti_symmetry; assumption.
+    assert(C' = B').
+      apply between_cong with A'.
+        assumption.
+        apply cong_3412; assumption.
+    split.
+      subst B'.
+      assert(Cong A B A C).
+        apply cong_1234_5634 with A' C'; assumption.
+      apply between_cong with A; assumption.
+    apply eq_sym. assumption.
+Qed.
+
 End T5.
 
 Print All.
