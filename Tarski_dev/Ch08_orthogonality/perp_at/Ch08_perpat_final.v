0bf6a1dc0ee86a59db9f303fe6232c114b70dd48
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-12-07 09:47:27 +0100

    Create Ch08_perpat_final.v

---
+++ Tarski_dev/Ch08_orthogonality/perp_at/Ch08_perpat_final.v
@@ -0,0 +1,10 @@
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_eq.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_col.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_per.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_diff.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_not.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_preserved.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_cong.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_midpoint.
+(* Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp_at.Ch08_perpat_tactics. *)
