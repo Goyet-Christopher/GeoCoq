81802481a90b52446a49212a836baf48fb26c8b0
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-12-07 09:39:45 +0100

    Create Ch08_per_final.v

---
+++ Tarski_dev/Ch08_orthogonality/per/Ch08_per_final.v
@@ -0,0 +1,9 @@
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per_exists.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per_col.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per_diff.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per_preserved.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per_not_col.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per_cong.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per_midpoint.
+(*Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.per.Ch08_per_tactics. *)
