ddd60f3ff843cdb3821b40b3decbe748b200cb06
GOYET Christopher <github_chris@matheuxfous.xyz>
2020-12-11 21:36:00 +0100

    Create Ch08_final.v

---
+++ Tarski_dev/Ch08_orthogonality/Ch08_final.v
@@ -0,0 +1,4 @@
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.perp.Ch08_perp_final.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.Ch08_decidability.
+Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.Ch08_midpoint_exists.
+(* Require Export GeoCoq.Tarski_dev.Ch08_orthogonality.Ch08_tactics.v. *)
